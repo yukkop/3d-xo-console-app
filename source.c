20e7ce6dec86eeebc04070f7b6b27e121de8fc40
yukkop <yukkop2@mail.ru>
2022-04-03 17:22:14 +0300

    feat: impliment tail sellecting

--- source.c
+++ source.c
@@ -32,7 +32,13 @@ typedef struct
     int yOffset;
 } Camera;
 
-void renderScreen(Camera screen, char field[], int width, int height)
+typedef struct
+{
+    int x;
+    int y;
+} PointInt;
+
+void renderOnScreen(Camera *camera, char field[], int width, int height)
 {
     // printf("height: %i, width: %i \n", height, width);
     /*
@@ -45,24 +51,36 @@ void renderScreen(Camera screen, char field[], int width, int height)
         printf("\n");
     }
     */
-    for (int y = 0; y < screen.height - 1; y++)
+    for (int y = 0; y < camera->height; y++)
     {
         if (y >= height)
-            for (int x = 0; x < screen.width; x++)
+            for (int x = 0; x < camera->width; x++)
             {
                 printf("%s", VOID_SYMBOL);
             }
         else
-            for (int x = 0; x < screen.width * UNICOD_COSTIL; x++)
+            for (int x = 0; x < camera->width * UNICOD_COSTIL; x++)
             {
                 if (x >= width)
                     continue;
                 else
-                    printf("%c", field[screen.xOffset + x + (screen.yOffset + y) * width]);
+                    printf("%c", field[camera->xOffset + x + (camera->yOffset + y) * width]);
             }
-        if (y <= screen.height - 1)
+        if (y <= camera->height - 2)
             printf("\n");
     }
+}
+
+void render(char *field, Camera *camera, struct winsize *w)
+{
+    system("clear");
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, w);
+    camera->xOffset = 0;
+    camera->yOffset = 0;
+    camera->width = w->ws_col;
+    camera->height = w->ws_row;
+
+    renderOnScreen(camera, field, FIELD_WIDTH * TILE_WIDTH, FIELD_HEIGHT * TILE_HEIGHT);
 }
 
 void cutTileSHeetToTiles(Tile *tiles, char *tileSheet)
@@ -103,6 +121,24 @@ void inputTileToScreen(Tile *tile, int x, int y, char *screen)
     }
 }
 
+void initField(Tile *defaultTiles, char *field)
+{
+    for (int i = 0; i < FIELD_HEIGHT * FIELD_WIDTH; i++)
+    {
+        int x = i % FIELD_WIDTH;
+        int y = i / FIELD_WIDTH;
+        // printf("%i:%i\n", x, y);
+        inputTileToScreen(&defaultTiles[i], x, y, field);
+    }
+}
+
+void put();
+
+Tile emptyTiles[FIELD_HEIGHT * FIELD_WIDTH];
+Tile xTiles[FIELD_HEIGHT * FIELD_WIDTH];
+Tile oTiles[FIELD_HEIGHT * FIELD_WIDTH];
+Tile chosenTile[FIELD_HEIGHT * FIELD_WIDTH];
+
 int main(int argc, char *argv[])
 {
     struct winsize w;
@@ -110,37 +146,118 @@ int main(int argc, char *argv[])
     int fieldSize = FIELD_HEIGHT * FIELD_WIDTH * TILE_HEIGHT * TILE_WIDTH;
     char field[fieldSize];
 
-    Tile emptyTiles[FIELD_HEIGHT * FIELD_WIDTH];
-    Tile xTiles[FIELD_HEIGHT * FIELD_WIDTH];
-    Tile oTiles[FIELD_HEIGHT * FIELD_WIDTH];
-
     cutTileSHeetToTiles(emptyTiles, EMPTY_TILES);
     cutTileSHeetToTiles(oTiles, O_TILES);
     cutTileSHeetToTiles(xTiles, X_TILES);
+    cutTileSHeetToTiles(chosenTile, CHOSEN_TILES);
 
-    for (int i = 0; i < FIELD_HEIGHT * FIELD_WIDTH; i++)
+    initField(emptyTiles, field);
+
+    Camera camera;
+    PointInt selectedTilePoint;
+    selectedTilePoint.x = 1;
+    selectedTilePoint.y = 4;
+
+    char command = 'q';
+
+    render(field, &camera, &w);
+    do
     {
-        int x = i % FIELD_WIDTH;
-        int y = i / FIELD_WIDTH;
-        // printf("%i:%i\n", x, y);
-        inputTileToScreen(&emptyTiles[i], x, y, field);
-    }
+        command = getch();
 
-    inputTileToScreen(&xTiles[1 + 1 * FIELD_WIDTH], 1, 1, field);
-    inputTileToScreen(&oTiles[0 + 2 * FIELD_WIDTH], 0, 2, field);
+        switch (command)
+        {
+        case 'p':
+        case 'P':
+            put(&selectedTilePoint, field, &camera, &w);
 
-    while (1)
+            break;
+        case 'r':
+        case 'R':
+            break;
+        default:
+            continue;
+        }
+
+        render(field, &camera, &w);
+    } while (command != 'q');
+    system("clear");
+}
+
+void chageSelectedTile(PointInt *selectedTilePoint, char *field, int newX, int newY)
+{
+    inputTileToScreen(
+        &emptyTiles[selectedTilePoint->x + selectedTilePoint->y * FIELD_WIDTH],
+        selectedTilePoint->x,
+        selectedTilePoint->y,
+        field);
+
+    selectedTilePoint->x = newX;
+    selectedTilePoint->y = newY;
+
+    inputTileToScreen(
+        &chosenTile[selectedTilePoint->x + selectedTilePoint->y * FIELD_WIDTH],
+        selectedTilePoint->x,
+        selectedTilePoint->y,
+        field);
+}
+
+void put(PointInt *selectedTilePoint, char *field, Camera *camera, struct winsize *w)
+{
+    char command = 'q';
+    inputTileToScreen(
+        &chosenTile[selectedTilePoint->x + selectedTilePoint->y * FIELD_WIDTH],
+        selectedTilePoint->x,
+        selectedTilePoint->y,
+        field);
+
+    render(field, camera, w);
+    do
     {
-        system("clear");
-        ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-        Camera camera;
-        camera.xOffset = 0;
-        camera.yOffset = 0;
-        camera.width = w.ws_col;
-        camera.height = w.ws_row;
+        command = getch();
+        switch (command)
+        {
+        case 37: // LeftArrow
+        case 'a':
+            chageSelectedTile(selectedTilePoint, field, selectedTilePoint->x - 1, selectedTilePoint->y);
+            break;
+        case 38: // UpArrow
+        case 'w':
+            chageSelectedTile(selectedTilePoint, field, selectedTilePoint->x, selectedTilePoint->y - 1);
+            break;
+        case 39: // RightArrow
+        case 'd':
+            chageSelectedTile(selectedTilePoint, field, selectedTilePoint->x + 1, selectedTilePoint->y);
+            break;
+        case 40: // DowndArrow
+        case 's':
+            chageSelectedTile(selectedTilePoint, field, selectedTilePoint->x, selectedTilePoint->y + 1);
+            break;
+        case 'o':
+        case 'O':
+            inputTileToScreen(
+                &oTiles[selectedTilePoint->x + selectedTilePoint->y * FIELD_WIDTH],
+                selectedTilePoint->x,
+                selectedTilePoint->y,
+                field);
+            return;
+        case 'x':
+        case 'X':
+            inputTileToScreen(
+                &xTiles[selectedTilePoint->x + selectedTilePoint->y * FIELD_WIDTH],
+                selectedTilePoint->x,
+                selectedTilePoint->y,
+                field);
+            return;
+        default:
+            continue;
+        }
+        render(field, camera, w);
+    } while (command != 'q');
 
-        renderScreen(camera, field, FIELD_WIDTH * TILE_WIDTH, FIELD_HEIGHT * TILE_HEIGHT);
-
-        getch();
-    }
+    inputTileToScreen(
+        &emptyTiles[selectedTilePoint->x + selectedTilePoint->y * FIELD_WIDTH],
+        selectedTilePoint->x,
+        selectedTilePoint->y,
+        field);
 }
